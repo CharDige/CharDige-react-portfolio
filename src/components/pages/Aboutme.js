df65d54ab2a45527491c42457fc3548fa5f5909b
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-30 21:13:22 +0930

    Basic styling and adding About me content from previous portfolio

---
+++ src/components/pages/Aboutme.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Aboutme() {
+    return (
+        <div>
+            <h2>
+                About me
+            </h2>
+            <p>
+                Full Stack web developer with extensive experience in communications, user experience, and project and change management.
+                {"\n"}
+                {"\n"}
+                Initially educated at the University of South Australia, completing a Bachelor of Journalism & Professional Writing and a Bachelor of Arts (English & Creative Writing), I have backed this up by currently studying with the University of Adelaide Full Stack Boot Camp.
+                {"\n"}
+                {"\n"}
+                Professional experience extends within industries including utilities, insurance, and government—specifically the emergency services—working in areas of communications and digital media for over four years.
+                {"\n"}
+                {"\n"}
+                Passionate about accessibility and delivering seamless user experiences, I'm excited to bring my two highly-sought for qualified backgrounds, and skills in time-management and collaboration, to provide web applications that are designed to meet user needs while also delivering clean and readable web copy.
+                {"\n"}
+                {"\n"}
+                Here, you can view my portfolio of web applications, as well as options on how you can contact me.
+            </p>
+        </div>
+    )
+}
