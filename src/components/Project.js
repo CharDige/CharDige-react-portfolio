d2c0f2f4691b9c208beb9cc3b9588396291c6fe3
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-30 20:40:43 +0930

    Imported all components and pages required

---
+++ src/components/Project.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import Navigation from './Navigation';
+import Aboutme from './pages/Aboutme';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Footer from './Footer';
