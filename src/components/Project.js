e655da3a6526f760d43214d300b1cc2952170eaf
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-30 20:59:27 +0930

    Added pages to render and use state for Aboutme to be the default page

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,3 +6,33 @@ import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 import Footer from './Footer';
+
+export default function Project() {
+    const [currentPage, setCurrentPage] = useState('Aboutme');
+
+    // Setting the value of currentPage so the pages render when selected in the nav
+    const renderPage = () => {
+        if (currentPage === 'Aboutme') {
+            return <Aboutme />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />;
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Header />
+            <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+            <Footer />
+        </div>
+    )
+}
