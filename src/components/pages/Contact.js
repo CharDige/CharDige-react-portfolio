623687745ea3375e8b3132dd8574316a40684bc1
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-31 22:46:56 +0930

    Added in styling and content prior to implementing form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,13 +2,22 @@ import React from 'react';
 
 export default function Contact() {
     return (
-        <div>
-            <h2>
-                Contact
-            </h2>
-            <p>
-                Contact me at <a href="mailto:chardige23@gmail.com">chardige23@gmail</a>
-            </p>
+        <div className='row' id='margin-style'>
+            <div className='col-12 col-md-3'>
+                <h2 className='heading-style'>
+                    Contact
+                </h2>
+            </div>
+
+            <div className='col-12 col-md-9 left-border'>
+                <p>
+                    Fill out and submit the below form to get into contact with me.
+                    <br></br>
+                    <br></br>
+                    Alternatively, you can contact me via email at <a href="mailto:chardige23@gmail.com">chardige23@gmail</a>
+                </p>
+
+            </div>
         </div>
     )
 }
