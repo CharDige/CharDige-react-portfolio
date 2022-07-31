521658020ec9df51ea074915c2e75c7fc8036b29
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-31 19:23:43 +0930

    Updating class names for styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import '../styles/Navigation.css';
 
 function Navigation({ currentPage, handlePageChange }) {
     return (
-        <nav className='navbar navbar-expand-lg'>
+        <nav className='navbar navbar-expand-lg nav-color'>
             <div className='container-fluid nav-center'>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span>Menu</span>
@@ -12,22 +12,22 @@ function Navigation({ currentPage, handlePageChange }) {
                     <div className='navbar-nav'>
                         <a href='#Aboutme'
                             onClick={() => handlePageChange('Aboutme')}
-                            className={currentPage === 'Aboutme' ? 'nav-link active' : 'nav-link'}>
+                            className={currentPage === 'Aboutme' ? 'nav-link nav-link-style-active' : 'nav-link nav-link-style'}>
                             About Me
                         </a>
                         <a href='#Portfolio'
                             onClick={() => handlePageChange('Portfolio')}
-                            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                            className={currentPage === 'Portfolio' ? 'nav-link nav-link-style-active' : 'nav-link nav-link-style'}>
                             Portfolio
                         </a>
                         <a href='#Contact'
                             onClick={() => handlePageChange('Contact')}
-                            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                            className={currentPage === 'Contact' ? 'nav-link nav-link-style-active' : 'nav-link nav-link-style'}>
                             Contact
                         </a>
                         <a href='#Resume'
                             onClick={() => handlePageChange('Resume')}
-                            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                            className={currentPage === 'Resume' ? 'nav-link nav-link-style-active' : 'nav-link nav-link-style'}>
                             Resume
                         </a>
                     </div>
