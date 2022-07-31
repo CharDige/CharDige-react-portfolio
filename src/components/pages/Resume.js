485d41c78a1a1dd9960e9293c845611dd0f8e608
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-31 22:31:08 +0930

    Added resume and classes for styling

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,14 +1,63 @@
 import React from 'react';
+import resume from '../../assets/charlotte-d-resume.pdf';
 
 export default function Resume() {
     return (
-        <div>
-            <h2>
-                Resume
-            </h2>
-            <p>
-                Content here for Resume
-            </p>
+        <div className='row' id='margin-style'>
+            <div className='col-12 col-md-3'>
+                <h2 className='heading-style'>
+                    Resume
+                </h2>
+            </div>
+
+            <div className='col-12 col-md-9 left-border'>
+                <p>
+                    <a href={resume} target='_blank' rel="noreferrer">Download a copy of my resume</a>.
+                    <br></br>
+                    <br></br>
+                    <h3>
+                        Coding languages and tools
+                    </h3>
+                    <ul>
+                        <li>
+                            HTML
+                        </li>
+                        <li>
+                            CSS
+                        </li>
+                        <li>
+                            JavaScript
+                        </li>
+                        <li>
+                            jQuery
+                        </li>
+                        <li>
+                            Bootstrap
+                        </li>
+                        <li>
+                            Express.js
+                        </li>
+                        <li>
+                            Node.js
+                        </li>
+                        <li>
+                            Handlebars
+                        </li>
+                        <li>
+                            MySQL
+                        </li>
+                        <li>
+                            Heroku
+                        </li>
+                        <li>
+                            GraphQL
+                        </li>
+                        <li>
+                            MongoDB
+                        </li>
+                    </ul>
+                </p>
+            </div>
         </div>
     )
 }
