1b51d751caf1abb875c07114951d47515e1d2d12
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-07-31 20:56:45 +0930

    Added icons, links and copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,19 @@ import '../styles/Footer.css';
 function Footer() {
     return (
         <footer className='footer'>
-            <p>
-                This is the footer
-            </p>
+            <a href="https://github.com/CharDige" target="_blank" rel="noreferrer" className='icon-link-style'>
+                <i className="bi bi-github icon-style"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/charlotte-dige/" target="_blank" rel="noreferrer" className='icon-link-style'>
+                <i className="bi bi-linkedin icon-style"></i>
+            </a>
+            <a href="https://www.instagram.com/charlotte_dige/" target="_blank" rel="noreferrer" className='icon-link-style'>
+                <i className="bi bi-instagram icon-style"></i>
+            </a>
+            <a href="https://stackoverflow.com/users/18213516/charlotte-d" target="_blank" rel="noreferrer" className='icon-link-style'>
+                <i className="bi bi-stack-overflow icon-style"></i>
+            </a>
+            <p>&copy; Charlotte Dige 2022</p>
         </footer>
     )
 }
