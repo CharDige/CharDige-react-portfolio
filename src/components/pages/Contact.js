4770b97db837788da6af6afe3975141e92b8aedf
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-01 17:56:37 +0930

    Added code for form submission prior to completing form code

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
+    // Variables for form fields and making their initial values and empty string
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    // Handling the input value to trigger the change
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        // Based on input, we set the state of the input
+        if (inputType === 'name') {
+            setName(inputValue);
+        } else if (inputType === 'email') {
+            setEmail(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+    // Handle the form submission
+    const handleFormSubmit = (e) => {
+        // Prevent the default behaviour
+        e.preventDefault();
+
+        // Validate the email
+        if(!validateEmail(email)) {
+            setErrorMessage("Oops! Your email doesn't appear to be valid. How about another one?")
+            return;
+        }
+
+        // Check if name or message fields have any input
+        if (!name || !email || !message) {
+            setErrorMessage("You've missed a spot! Please fill out all fields");
+            return;
+        }
+
+        // Log to the console that it's been submitted
+        console.log('Form submitted!');
+
+        // Clear out the input field after submitted
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+
     return (
         <div className='row' id='margin-style'>
             <div className='col-12 col-md-3'>
@@ -11,10 +60,7 @@ export default function Contact() {
 
             <div className='col-12 col-md-9 left-border'>
                 <p>
-                    Fill out and submit the below form to get into contact with me.
-                    <br></br>
-                    <br></br>
-                    Alternatively, you can contact me via email at <a href="mailto:chardige23@gmail.com">chardige23@gmail</a>
+                    Fill out and submit the below form to get into contact with me. Or, flick me an email at <a href="mailto:chardige23@gmail.com">chardige23@gmail</a>
                 </p>
 
             </div>
